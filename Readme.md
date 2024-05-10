d49fb2942533df395861728016c0f5d6f49e3344
Ana Carolina <anactavaresbrand@gmail.com>
2024-05-10 20:51:19 -0300

    parte 2

--- Readme.md
+++ Readme.md
@@ -13,13 +13,20 @@ Um pequeno hospital local busca desenvolver um novo sistema que atenda melhor à
 `•Convênios:` nome, CNPJ e tempo de carência.
 
 ## Consultas
-•Registro de consultas realizadas: data e hora, médico responsável, paciente.
-•Valor da consulta ou convênio utilizado (com número da carteira).
-•Indicação da especialidade buscada pelo paciente.
-•Receitas
-•Registro de medicamentos prescritos, quantidade e instruções de uso.
+`•Registro de consultas realizadas:` data e hora, médico responsável, paciente.
+`•Valor da consulta ou convênio utilizado` (com número da carteira).
+`•Indicação da especialidade buscada pelo paciente.`
+`•Receitas`
+`•Registro de medicamentos prescritos, quantidade e instruções de uso.`
 
 <img src="/diagrama.png">
+
+# Exercicio 2
+Faça a ligação do diagrama acima ao diagrama desenvolvido na atividade antrior, construindo relacionamentos com entidades relacionadas. E eleve o seu diagrama para que já selecionando os tipos de dados que serão trabalhados e em quais situações. 
+
+Por último, crie um script SQL para a geração do banco de dados e para instruções de montagem de cada uma das entidades/tabelas presentes no diagrama completo (considerando as entidades do diagrama da atividade anterior e as novas entidades propostas no diagrama acima). Também crie tabelas para relacionamentos quando necessário. Aplique colunas e chaves primárias e estrangeiras.
+
+<img src="/2.png">
 
 
 # ⚒️ Tecnologias ultilizadas 
--- hospital-fundamental.sql
+++ hospital-fundamental.sql
@@ -1,9 +1,10 @@
 CREATE TABLE Medico (
-	id_medico number(10,0),
+	id_medico number (10,0),
 	especialidade varchar2(255) NOT NULL,
 	crm varchar2(255) NOT NULL,
 	CONSTRAINT Medico PRIMARY KEY (id_medico)
 );
+
 
 CREATE TABLE Paciente (
 	id_paciente number(10,0),
@@ -18,7 +19,7 @@ CREATE TABLE Paciente (
 );
 
 CREATE TABLE Consultas (
-	id_consultas number(10,0),
+	id_consultas NUMBER(10,0),
 	hora_consulta varchar2(255) NOT NULL,
 	medico_responsavel varchar2(255) NOT NULL,
 	valor_consulta number(10) NOT NULL,
@@ -28,16 +29,60 @@ CREATE TABLE Consultas (
 ); 
 
 CREATE TABLE Receita_medico (
-	id_receita number(10,0) ,
+	id_receita number(10,0)PRIMARY KEY,
 	nome_paciente varchar2(255) NOT NULL,
 	medicamentos varchar2(255) NOT NULL,
 	CONSTRAINT Receita_medico PRIMARY KEY (id_receita)
 );
 
 CREATE TABLE Convenio (
-	id_convenio number(10,0),
+	id_convenio NUMBER (10,0),
 	nome_convenio varchar2(255) NOT NULL,
 	cnpj number(10) NOT NULL,
 	tempo_carencia varchar2(255) NOT NULL,
 	CONSTRAINT Convenio PRIMARY KEY (id_convenio)
 );
+
+CREATE TABLE Enfermeiro(
+    id_enferemeiro NUMBER(10,0)PRIMARY KEY,
+    nome varchar(255),
+    cre VARCHAR(255),
+    cpf varchar(255)
+);
+
+CREATE TABLE Internacao(
+    id_internacao NUMBER(10,2)PRIMARY KEY,
+    data_entrada date NOT NULL,
+    data_prev_alta date NOT NULL,
+    data_alta date NOT NULL,
+    procedimento varchar(255)
+);    
+
+CREATE TABLE Quarto(
+    id_quarto number PRIMARY KEY,
+    numero_quarto varchar(255),
+    valor_diaria number(10,2),
+    descricao varchar(255),
+    procedimento varchar(255)
+);
+
+CREATE TABLE tipo_quarto(
+    id_tipo int PRIMARY KEY,
+    descricao varchar(255),
+    valor_diaria number (10,2)
+);    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
